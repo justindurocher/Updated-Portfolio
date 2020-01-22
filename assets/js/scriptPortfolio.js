589b7102fb0aae9f7189606be7463497a569b4e6
Justin Durocher <justduro@gmail.com>
2020-01-22 16:28:41 -0500

    Hover on name and buttons with jquery

---
+++ assets/js/scriptPortfolio.js
@@ -0,0 +1,61 @@
+$("#day-planning").hover(
+
+    function(){
+
+        $(".day-planner").css("font-size" , "20px");
+    
+    }, 
+    
+    function(){
+    
+        $(".day-planner").css("font-size" , "16px");
+
+});
+
+$("#video-gaming").hover(
+
+    function(){
+
+        $(".video-game").css("font-size" , "20px");
+        $(".video-game").css("width" , "240px");
+        $(".video-game").css("left" , "60px");
+    
+    }, 
+    
+    function(){
+    
+        $(".video-game").css("font-size" , "16px");
+        $(".video-game").css("width" , "200px");
+        $(".video-game").css("left" , "");
+
+    });
+
+$("#javascripting").hover(
+
+    function(){
+
+        $(".javascript-quiz").css("font-size" , "20px");
+    
+    }, 
+    
+    function(){
+    
+        $(".javascript-quiz").css("font-size" , "16px");
+
+    });
+
+$("#phone").hover(
+
+    function(){
+
+        $(".rock-paper-scissors").css("font-size" , "20px");
+
+    } ,
+
+    function(){
+
+        $(".rock-paper-scissors").css("font-size" , "16px");
+
+    }
+
+);
